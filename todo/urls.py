7a932a12d3a991617aea8f5ebe6079c8ed995242
Pedro Vilela <pedroh_mvilela@hotmail.com>
2022-01-24 19:07:39 -0300

    Todo delete view + url + htmx

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add_todo, todos, update_todo
+from .views import add_todo, todos, update_todo, delete_todo
 
 urlpatterns = [
     path('', todos, name='todos'),
     path('add-todo/', add_todo, name='add-todo'),
-    path('update/<int:pk>/', update_todo, name='update-todo')
+    path('update/<int:pk>/', update_todo, name='update-todo'),
+    path('delete-todo/<int:pk>/', delete_todo, name='delete-todo')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
@@ -27,3 +28,11 @@ def update_todo(request, pk):
     todo.save()
 
     return render(request, 'todo/partials/todo.html', {'todo': todo})
+
+
+@require_http_methods(['DELETE'])
+def delete_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.delete()
+
+    return HttpResponse()
